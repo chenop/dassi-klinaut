9cd7eff3ce6c58c62f4539fa23ddddebe656df01
Chen Oppenhaim <chen.oppenhaim@toluna.com>
2019-11-10 01:50:24 +0200

    Updates

---
+++ precache-manifest.5d82083fc71a0fa146c4657d5935524d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef377e2144622d4d98bafc6fe3b3f6ff",
+    "url": "/dassi-klinaut/index.html"
+  },
+  {
+    "revision": "5e77525ab3d8a18fda3f",
+    "url": "/dassi-klinaut/static/css/main.2110a780.chunk.css"
+  },
+  {
+    "revision": "a493b11a5dc3eb8740a6",
+    "url": "/dassi-klinaut/static/js/2.58c9cfc1.chunk.js"
+  },
+  {
+    "revision": "5e77525ab3d8a18fda3f",
+    "url": "/dassi-klinaut/static/js/main.7940fd1b.chunk.js"
+  },
+  {
+    "revision": "73a2bdd5ddbae8f0bfd9",
+    "url": "/dassi-klinaut/static/js/runtime-main.edc2e00e.js"
+  },
+  {
+    "revision": "c4de7e96a2f52144b11e5ba7daea6cac",
+    "url": "/dassi-klinaut/static/media/main.c4de7e96.jpg"
+  }
+]);
